1d5a7ec521b2eb959135c65dff7d1307e50ec042
rib9985 <rib9985@gmail.com>
2024-06-26 14:44:27 -0300

    Create basic class

--- hashFunction.mjs
+++ hashFunction.mjs
@@ -1,1 +1,25 @@
-export default function hash(key) {}
+// Hash function based on Donald E Knuth hash -> Art of Programming
+export default function hashFunction(string) {
+  let length = string.length;
+  let hash = length;
+  let i = 0;
+
+  for (i = 0; i < length; i++) {
+    hash = (hash << 5) ^ (hash >> 27) ^ string[i] % 16;
+  }
+  return hash;
+}
+
+/* Testing for the hash function
+
+console.log(hashFunction("hello123"));
+console.log(hashFunction("123"));
+console.log(hashFunction("hello"));
+console.log(hashFunction("123"));
+
+*/
+console.log(hashFunction("hello12asdfasdfasdf12341"));
+console.log(hashFunction("123"));
+console.log(hashFunction("hello"));
+console.log(hashFunction("123"));
+console.log(hashFunction("12312"));
--- hashmap.mjs
+++ hashmap.mjs
@@ -1,1 +1,36 @@
-export default class Hashmap {}
+import hashFunction from "./hashFunction.mjs";
+export default class Hashmap {
+  constructor(size) {
+    this.buckets = [];
+    this.bucketSize = size;
+    this.loadFactor = Math.ceil(this.buckets.length / size);
+  }
+
+  accessBucket(index) {
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access an index out of bound");
+    }
+    return index;
+  }
+
+  hash(key) {
+    let keyToHash = key;
+    if ((key = !typeof String)) {
+      keyToHash = key.toString();
+    }
+    if (key == typeof null) {
+      keyToHash = "null";
+    }
+    const hash = hashFunction(keyToHash);
+    return hash % 16;
+  }
+
+  set(key, value) {
+    const position = this.accessBucket(key);
+    if (position) {
+      this.buckets[position] = value;
+    }
+  }
+
+  get(key) {}
+}
